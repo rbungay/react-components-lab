18f154c6493742f2f2da53c5250c77c4d8cc8b2f
Rani Bungay <ranibungay@gmail.com>
2024-10-12 18:02:34 -0400

    create weather component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,53 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import WeatherForecast from "./components/WeatherForecast/WeatherForecast";
 import "./App.css";
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const weatherForecasts = [
+    {
+      day: "Mon",
+      img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/day.svg",
+      imgAlt: "sun icon",
+      conditions: "sunny",
+      time: "Morning",
+    },
+    {
+      day: "Tue",
+      img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/night.svg",
+      imgAlt: "moon icon",
+      conditions: "clear",
+      time: "Night",
+    },
+    {
+      day: "Wed",
+      img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/stormy.svg",
+      imgAlt: "clouds with lightning icon",
+      conditions: "stormy",
+      time: "All Day",
+    },
+    {
+      day: "Thu",
+      img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-day_t7ckxp.svg",
+      imgAlt: "sun overcast by clouds icon",
+      conditions: "overcast",
+      time: "Evening",
+    },
+    {
+      day: "Fri",
+      img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-night.svg",
+      imgAlt: "moon overcast by clouds icon",
+      conditions: "cloudy",
+      time: "Night",
+    },
+  ];
+
+  return (
+    <>
+      <h1>Local Weather</h1>
+      <section>
+        <WeatherForecast />
+      </section>
+    </>
+  );
 };
 
 export default App;
